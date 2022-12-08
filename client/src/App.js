7543b4c7c3c60c1cb947f863c9bc013ffe28294e
funkycadet <ericsmithsonian2@gmail.com>
2022-12-08 13:48:01 +0100

    update: add client directory

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import {Header} from './components/header'
+import {LandingPage} from './components/landingPage'
+// import { useState } from 'react';
+
+function App() {
+  return (
+    <div className="App mx-14 my-6 w-100%">
+      <Header/>
+      <LandingPage/>
+    </div>
+  );
+}
+
+export default App;
